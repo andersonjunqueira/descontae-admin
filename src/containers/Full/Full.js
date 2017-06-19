2450b17b048c68412699f43d3af82a16defb56af
Anderson Junqueira <acosorio@stefanini.com>
2017-06-19 13:28:25 -0300

    ajustando o tema do projeto

--- src/containers/Full/Full.js
+++ src/containers/Full/Full.js
@@ -15,23 +15,21 @@ class Full extends Component {
         <div className="app">
             <Header />
             <div className="app-body">
-              <Sidebar {...this.props}/>
-              <main className="main">
-                  <Breadcrumbs wrapperElement="ol"
-                      wrapperClass="breadcrumb"
-                      itemClass="breadcrumb-item"
-                      separator=""
-                      routes={this.props.routes}
-                      params={this.props.params}
-                  />
-                  <div className="container-fluid">{this.props.children}</div>
-              </main>
-              <Aside />
+                <Sidebar {...this.props}/>
+                <main className="main">
+                    {/*<Breadcrumbs wrapperElement="ol"
+                        wrapperClass="breadcrumb"
+                        itemClass="breadcrumb-item"
+                        separator=""
+                        routes={this.props.routes}
+                        params={this.props.params}
+                    />*/}
+                    <div className="container-fluid">{this.props.children}</div>
+                </main>
+                <Aside />
             </div>
             <Footer />
-
             <NotificationsSystem theme={theme} />
-
          </div>
         );
     }
