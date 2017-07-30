7f9869c428862aae61cbb0058dc8372883d7ca08
acosorio <anderson.junqueira@gmail.com>
2017-07-30 10:43:59 -0300

    ok

--- src/modules/Inicio/Inicio.js
+++ src/modules/Inicio/Inicio.js
@@ -6,17 +6,10 @@ import Mask from '../../components/Mask';
 
 class Inicio extends Component {
 
-    doSearch() {
-        console.log("OK");
-    }
-
     render() {
         return (
         <div>
             <h4>Inicio</h4>
-            <Form>
-                <Mask name="mask" label="MASK" required={true} mask="1111 1111"/>
-            </Form>
         </div>
         );
     }
