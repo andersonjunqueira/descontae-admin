5f9ad7b463d3fb00f192fe435e56d5051bca3aec
acosorio <anderson.junqueira@gmail.com>
2017-11-25 02:00:13 -0200

    configuração do servico dinamico Filtros do consumo

--- src/modules/Consumo/Consumo.actions.js
+++ src/modules/Consumo/Consumo.actions.js
@@ -1,17 +1,33 @@
 import axios from "axios";
 
+import { dateFunctions } from '../../components/Date';
 import { toaster } from '../../components/Notification/Notification.actions';
+import { translate } from '../../components/Intl/Intl.actions';
 
 export const [ CONSUMOS_PESQUISA, CONSUMO_EDICAO, CONSUMO_SETMODE ] = [ "CONSUMOS_PESQUISA", "CONSUMO_EDICAO", "CONSUMO_SETMODE" ];
 
 export const consultar = (filtro, start, pagesize) => {
-    filtro = filtro ? filtro : {};
-    filtro.start = start;
-    filtro.page = pagesize;
+
+    let qs = '';
+    if(filtro.cliente) {
+        qs += `cliente=${filtro.cliente}`;
+    }
+
+    if(filtro.cidade && filtro.cidade !== translate('selecione')) {
+        qs += `${qs.length > 0 ? '&' : ''}cidade=${filtro.cidade}`;
+    }
+
+    if(filtro.dataInicio) {
+        qs += `${qs.length > 0 ? '&' : ''}dataInicio=${dateFunctions.toBackend(filtro.dataInicio)}`;
+    }
+
+    if(filtro.dataFim) {
+        qs += `${qs.length > 0 ? '&' : ''}dataFim=${dateFunctions.toBackend(filtro.dataFim)}`;
+    }
 
     return dispatch => {
 
-        axios.get('/consumos', { params: filtro })
+        axios.get(`/consumos${qs.length > 0 ? '?' : ''}${qs}`)
             .then(function(response) {
                 dispatch({type: CONSUMOS_PESQUISA, payload: response.data});
 
